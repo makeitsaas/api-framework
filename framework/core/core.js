847d03e25c0c97dfc24e99c9566785a901eb501a
Duwab <contact@superpaas.pro>
2019-03-25 18:25:19 +0100

    Enable CORS

--- framework/core/core.js
+++ framework/core/core.js
@@ -6,6 +6,12 @@ module.exports = function(app){
   const config = YAML.load('./app/config/config.yml');
   const builder = require('./core-builder')(app, config);
 
+  app.use(function(req, res, next) {
+    // CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
   builder.configureRoutes();
   framework.queue = builder.configureQueue();
