4bf3160da8ad3ba854a05c5b6b7b0812d0cefa37
Duwab <contact@superpaas.pro>
2019-05-04 21:27:53 +0200

    Remove path debug

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 let rootPath = require.main.filename.replace(/\/app\/app\.js$/, '');
-console.log("filename from module", rootPath);
 process.env.__PROJECT_ROOT_PATH__ = rootPath;
 module.exports = require('./dist/core/core');
